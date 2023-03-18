0d51651dea1850aedc1383ce0a8e53c2175652b6
Elia Zanotti <ELIA.ZANOTTI@local.ittcesena.it>
2023-03-18 09:10:09 +0100

    versione 1 della classe alunno: metodo aggiungi alunno

---
+++ verificaRecupero/scuola.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace verificaRecupero
+{
+    internal class scuola
+    {
+        public string nomeScuola { get; set; }
+        public string indirizzoScuola { get; set; }
+        public string orarioApertura { get; set; }
+        public string orarioChiusura { get; set; }
+
+        private list<alunno> alunni = new list<alunno>();
+
+        public void aggiungiAlunno(alunno alunno)
+        {
+            alunni.Add(alunno);
+        }
+    }
+}
